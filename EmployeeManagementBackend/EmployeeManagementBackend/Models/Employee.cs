d712d8415844076e2989367ce23c0274f3811b5c
leonidas-theodoridis <ltheodor@hotmail.de>
2025-01-08 23:45:09 +0200

    Initial commit

---
+++ EmployeeManagementBackend/EmployeeManagementBackend/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagement.Models
+
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
+        public string Position { get; set; }
+
+    }
+}
+
+
+
+
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ EmployeeManagementBackend/EmployeeManagementBackend/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagementBackend.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task AddEmployeeAsync(Employee employee);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(int id);
+
+
+
+
+
+    }
+}
